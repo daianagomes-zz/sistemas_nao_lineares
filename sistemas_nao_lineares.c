b48d21e3d737eb0169a3c423a67b16d4f06f8bae
daianagomes <daianagomes@id.uff.br>
2017-12-20 10:09:59 -0200

    Função que calcula as derivadas que serão usadas no Jacobiano

--- sistemas_nao_lineares.c
+++ sistemas_nao_lineares.c
@@ -18,3 +18,23 @@ double f3(double x[N])
     return 3*x[0] - cos(x[1]*x[2]) - 1/2;
     
 }
+
+double df(double f(), double x[N], int k)     
+{
+    double dff, h, aux;
+
+    h = 1e-6;
+    
+    aux = x[k];                  
+    x[k] = x[k] + h/2.;
+    
+    dff = f(x);
+    
+    x[k] = x[k] - h;
+    dff = (dff - f(x))/h;
+    
+    x[k] = aux;
+    
+    return dff;
+
+}
